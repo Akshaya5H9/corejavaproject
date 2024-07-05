cc918418422cf2f80133655316c96f6c9386d6ea
Administrator <Administrator@ACL1LTP072.axiscl.com>
2024-07-05 23:02:35 +0530

    second commit

---
+++ corejavaprogram/src/com/siiet/encapsulation/Encapsulationdemo.java
@@ -0,0 +1,10 @@
+package com.siiet.encapsulation;
+
+public class Encapsulationdemo {
+	int serialnumber;
+	String name;
+	int age;
+	void show() {
+		System.out.println("serial number"+serialnumber+ "\t Name: "+name +"\t Age: "+age);
+	}
+}
---
+++ corejavaprogram/src/com/siiet/encapsulation/demo.java
@@ -0,0 +1,13 @@
+package com.siiet.encapsulation;
+
+public class demo {
+	public static void main(String [] args) {
+		Encapsulationdemo e=new Encapsulationdemo();
+		e.serialnumber =1;
+		e.name="akki";
+		e.age=20;
+		e.show();
+		
+	}
+
+}
