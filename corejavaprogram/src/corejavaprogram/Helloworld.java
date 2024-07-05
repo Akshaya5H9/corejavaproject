f33f54f18d7a184a213f843db23cc5f4634e9a70
Administrator <Administrator@ACL1LTP072.axiscl.com>
2024-07-05 13:02:05 +0530

    first commit

---
+++ corejavaprogram/src/corejavaprogram/Helloworld.java
@@ -0,0 +1,14 @@
+package corejavaprogram;
+
+import java.util.Scanner;
+
+public class Helloworld {
+	public static   void main(String args[]) {
+		System.out.println("Helloworld");
+		System.out.println("welcome to TNS");
+		
+		
+		
+	}
+
+}
