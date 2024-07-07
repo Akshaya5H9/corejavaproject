63af2e4c32c102c1200f9ced94899342f70b360e
Administrator <Administrator@ACL1LTP072.axiscl.com>
2024-07-07 23:24:43 +0530

    third commit

---
+++ corejavaprogram/src/com/siiet/Scannerclass/Scannerexample.java
@@ -0,0 +1,16 @@
+package com.siiet.Scannerclass;
+import java.util.Scanner;
+
+public class Scannerexample {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter your name");
+		String name=sc.nextLine();
+		System.out.println("enter the rollnumber");
+		int roll=sc.nextInt();
+		
+				;
+		
+	}
+
+}
